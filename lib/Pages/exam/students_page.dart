48ca8bcc678a4bcb7dd3bd7aa96bcfa7eae86837
NinhDucAnh <94469719+ducanhizme@users.noreply.github.com>
2024-04-11 08:26:40 +0700

    last

--- lib/Pages/exam/students_page.dart
+++ lib/Pages/exam/students_page.dart
@@ -1,5 +1,7 @@
+import 'package:english_learning/Pages/exam/students.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class StudentsScreen extends StatefulWidget {
   const StudentsScreen({super.key});
@@ -9,71 +11,119 @@ class StudentsScreen extends StatefulWidget {
 }
 
 class _StudentsScreenState extends State<StudentsScreen> {
+  final TextEditingController idController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController dobController = TextEditingController();
+  final TextEditingController genderController = TextEditingController();
+  final TextEditingController gmailController = TextEditingController();
+  final TextEditingController scoreController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-          title: Text('Students Screen')
+        title: Text('Students Screen'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: SingleChildScrollView(
-          child:Column(
-            children:[
-              Text('Students Information',style: GoogleFonts.roboto(fontSize: 17,fontWeight: FontWeight.bold),),
-              const TextField(
+          child: Column(
+            children: [
+              Text(
+                'Students Information',
+                style: GoogleFonts.roboto(fontSize: 17, fontWeight: FontWeight.bold),
+              ),
+              TextField(
+                controller: idController,
                 decoration: InputDecoration(
                   hintText: 'Enter Student ID',
+                  labelText: 'Student ID',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                controller: nameController,
+                decoration: InputDecoration(
+                  hintText: 'Enter Student Name',
                   labelText: 'Student Name',
-                  border: OutlineInputBorder()
+                  border: OutlineInputBorder(),
                 ),
               ),
-              SizedBox(height: 10,),
-              const TextField(
+              SizedBox(height: 10),
+              TextField(
+                controller: dobController,
                 decoration: InputDecoration(
-                    hintText: 'Enter Student Name',
-                    labelText: 'Student Name',
-                    border: OutlineInputBorder()
+                  hintText: 'Enter Student Date of Birth',
+                  labelText: 'Date of Birth',
+                  border: OutlineInputBorder(),
                 ),
               ),
-              SizedBox(height: 10,),
-              const TextField(
+              SizedBox(height: 10),
+              TextField(
+                controller: genderController,
                 decoration: InputDecoration(
-                    hintText: 'Enter Student Date of Birth',
-                    labelText: 'Date of Birth',
-                    border: OutlineInputBorder()
+                  hintText: 'Enter Student Gender',
+                  labelText: 'Gender',
+                  border: OutlineInputBorder(),
                 ),
               ),
-              SizedBox(height: 10,),
-              const TextField(
+              SizedBox(height: 10),
+              TextField(
+                controller: gmailController,
                 decoration: InputDecoration(
-                    hintText: 'Enter Student Gender',
-                    labelText: 'Gender',
-                    border: OutlineInputBorder()
+                  hintText: 'Enter Student Gmail',
+                  labelText: 'Gmail',
+                  border: OutlineInputBorder(),
                 ),
               ),
-              SizedBox(height: 10,),
-              const TextField(
+              SizedBox(height: 10),
+              TextField(
+                controller: scoreController,
                 decoration: InputDecoration(
-                    hintText: 'Enter Student Gmail',
-                    labelText: 'Gmail',
-                    border: OutlineInputBorder()
+                  hintText: 'Enter Student AVG Score',
+                  labelText: 'AVG Score',
+                  border: OutlineInputBorder(),
                 ),
               ),
-              SizedBox(height: 10,),
-              const TextField(
-                decoration: InputDecoration(
-                    hintText: 'Enter Student AVG Score',
-                    labelText: 'AVG Score',
-                    border: OutlineInputBorder()
-                ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  addStudentToFirebase();
+                },
+                child: Text('Submit'),
               ),
-            ]
-
-          )
+            ],
+          ),
         ),
-      )
+      ),
     );
   }
-}
 
+  Future<void> addStudentToFirebase() async {
+    String id = idController.text;
+    String name = nameController.text;
+    String address = ''; // Add address input if needed
+    DateTime birthDate = DateTime.parse(dobController.text);
+    String gender = genderController.text;
+    String gmail = gmailController.text;
+    double score = double.parse(scoreController.text);
+
+    Student student = Student(
+      id: id,
+      name: name,
+      address: address,
+      birthDate: birthDate,
+      gender: gender,
+      gmail: gmail,
+      score: score,
+    );
+
+    CollectionReference students = FirebaseFirestore.instance.collection('students');
+    return students
+        .doc(student.id)
+        .set(student.toMap())
+        .then((value) => print("Student Added"))
+        .catchError((error) => print("Failed to add student: $error"));
+  }
+}
